9c9c8c568a78131a90e5a6ac6d0b0ddc86e91210
Diogo Resende <dresende@thinkdigital.pt>
2011-12-21 22:31:53 +0000

    Expose applications and profiles by their name to module.exports if they don't colide with any already existing property.

---
+++ examples/exports.js
@@ -0,0 +1,17 @@
+var roles = require("roles");
+
+roles.addApplication("myapp", [ "readonly", "readwrite" ]);
+roles.addProfile("guest", [ "myapp.readonly" ]);
+roles.addProfile("admin", [ "myapp.*" ]);
+
+if (roles.admin.hasRoles("myapp.readwrite")) {
+	console.log("Administrator has readwrite to myapp");
+}
+
+if (!roles.guest.hasRoles("myapp.readwrite")) {
+	console.log("Guest does not have readwrite to myapp");
+}
+
+// you could do the same to access applications, like this:
+//
+// roles.myapp.method()
